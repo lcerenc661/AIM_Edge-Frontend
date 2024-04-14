f957e2abd59e467e9618943a6059da227f3cc811
root <root@DESKTOP-NQDBL4F>
2024-04-14 11:47:26 -0500

    Create logic fixed, login UI updated

--- src/pages/AuthLayout.tsx
+++ src/pages/AuthLayout.tsx
@@ -3,7 +3,7 @@ import { Outlet } from "react-router-dom";
 const AuthLayout = () => {
   return (
     <main className="flex justify-center">
-      <div className="md:w-full w-[350px] md:px-10">
+      <div className="md:w-full  w-[350px] md:px-10">
         <Outlet />
       </div>
     </main>
